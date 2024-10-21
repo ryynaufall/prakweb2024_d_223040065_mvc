44ee05000201a2b98ca8c784872f7ab8e4ef92e5
ryynaufall <121987025+ryynaufall@users.noreply.github.com>
2024-10-21 08:34:27 +0700

    Add files via upload

---
+++ Video 7/phpmvc/app/models/User_model.php
@@ -0,0 +1,9 @@
+<?php 
+
+class User_model {
+    private $nama = 'Rayyan';
+
+    public function getUser() {
+        return $this->nama;
+    }
+}
---
+++ Video 7/phpmvc/app/views/home/index.php
@@ -0,0 +1,4 @@
+<div class="container">
+    <h1>Selamat Datang di Website Saya</h1>
+    <p>Halo, naa saya <?= $data['nama']; ?></p>
+</div>
